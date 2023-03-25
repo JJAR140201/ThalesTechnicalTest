e294daa94b7cab3c856dd6b21ed72d2f8bdfd136
JJAR140201 <avilesroajuanjose@gmail.com>
2023-03-25 14:57:01 -0500

    The firts commit

---
+++ src/main/java/com/ThalesTechnicalTest/ThalesTechnicalTest/ThalesTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.ThalesTechnicalTest.ThalesTechnicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThalesTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThalesTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ThalesTechnicalTest/ThalesTechnicalTest/ThalesTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThalesTechnicalTest.ThalesTechnicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThalesTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
