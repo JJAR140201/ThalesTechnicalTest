850b7f675dbe2e7e5801db4ef512be2cc9997802
JJAR140201 <avilesroajuanjose@gmail.com>
2023-03-26 15:22:06 -0500

    We are using restTemplate so we must give it the address so that the application can start

--- src/main/java/com/ThalesTechnicalTest/ThalesTechnicalTest/ThalesTechnicalTestApplication.java
+++ src/main/java/com/ThalesTechnicalTest/ThalesTechnicalTest/ThalesTechnicalTestApplication.java
@@ -2,8 +2,10 @@ package com.ThalesTechnicalTest.ThalesTechnicalTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AppConfig.class)
 public class ThalesTechnicalTestApplication {
 
 	public static void main(String[] args) {
