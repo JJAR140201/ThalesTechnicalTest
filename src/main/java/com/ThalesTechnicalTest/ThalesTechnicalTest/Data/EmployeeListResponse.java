9f1739ce4e7d5ac99fd29e4d49e8a04592ebceb9
JJAR140201 <avilesroajuanjose@gmail.com>
2023-03-26 15:28:20 -0500

    EmployeeResponse and EmployeeListResponse are the files to get employee by id and employee list

---
+++ src/main/java/com/ThalesTechnicalTest/ThalesTechnicalTest/Data/EmployeeListResponse.java
@@ -0,0 +1,12 @@
+package com.ThalesTechnicalTest.ThalesTechnicalTest.Data;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class EmployeeListResponse {
+    private List<Employee> data;
+}
+
+
+
---
+++ src/main/java/com/ThalesTechnicalTest/ThalesTechnicalTest/Data/EmployeeResponse.java
@@ -0,0 +1,10 @@
+package com.ThalesTechnicalTest.ThalesTechnicalTest.Data;
+
+import lombok.Data;
+
+@Data
+public class EmployeeResponse {
+    private Employee data;
+}
+
+
